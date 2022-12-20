e5fe0dcca81ce1a005a8c33af7233a4fa8e62f49
MohammadAlSadawi <117226366+MohammadAlSadawi@users.noreply.github.com>
2022-12-20 22:12:44 +0300

    Add files via upload

---
+++ ViewModels/HomeVM.cs
@@ -0,0 +1,17 @@
+﻿using FirstProjectHallBooking.Models;
+using System.Collections.Generic;
+
+namespace FirstProjectHallBooking.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Hall> halls { get; set; }
+        public UserHall UserHall { get; set; }
+        public Home home { get; set; }
+        public Aboutu Aboutu { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Visa> visas { get; set; }
+        public Testimonial tests { get; set; }
+        public List<Testimonial> testimonials { get; set; }
+    }
+}
